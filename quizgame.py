d9cc573a9da86c1ef73e5c8163b80a3f3100a8b2
Hojae <ghwo6@naver.com>
2026-08-06 18:24:20 +0900

    FEAT:5개 퀴즈 데이터 추가완료

--- quizgame.py
+++ quizgame.py
@@ -12,9 +12,13 @@ class QuizGame:
         # 종료(메뉴5번)를 선택했을때 True를 반환함
         self.is_exit_key:bool = False
 
-        sampleQuiz = Quiz("저의 이름은?",["김호재","휴즈","효성","삼성"],1)
+        sampleQuiz1 = Quiz("저의 이름은?",["김호재","휴즈","효성","삼성"],1)
+        sampleQuiz2 = Quiz("코디세이의 인근역의 이름은?",["삼성역","개포동역","대모산입구역","수서역"],2)
+        sampleQuiz3 = Quiz("코디세이 AI 올일원 2기 오리엔테이션을 진행한 날은?",["260727","260627","260617","260330"],1)
+        sampleQuiz4 = Quiz("코디세이 서울 교육장의 캐빈5가 위치한 층은?",["2층","3층","4층","5층"],4)
+        sampleQuiz5 = Quiz("코디세이가 제공하는 AI 이름은?",["제미나이","X AI","클로드","네이토"],4)
 
-        self.quiz_list =[sampleQuiz]
+        self.quiz_list =[sampleQuiz1,sampleQuiz2,sampleQuiz3,sampleQuiz4,sampleQuiz5]
 
     # 메뉴1 문제를 풉니다.
     def quiz_solve(self):
@@ -28,6 +32,7 @@ class QuizGame:
         for i,li in enumerate(self.quiz_list):
             print(f"{i+1}번 문제")
             print(li.question_getter())
+        print()
 
 
     # 메뉴4 가장 높은 점수를 확인합니다.
@@ -39,7 +44,7 @@ class QuizGame:
         is_valid_input = False
         while not is_valid_input:
             select = input("원하시는 번호를 입력해 주세요.").strip()
-            
+            print()
             if select.isdigit:
                 if select is not "":
                     select = int(select)
@@ -48,7 +53,7 @@ class QuizGame:
                     else:
                         print("1부터 5까지의 숫자중에서 입력해주세요.")
                 else:
-                    print("'' 입력을 받지 못했습니다.")
+                    print("'공백' 입력을 받지 못했습니다.")
             else:
                 print("숫자를 입력해주세요.")
             self.menu_print()
@@ -61,7 +66,7 @@ class QuizGame:
         print("-----3. 퀴즈 목록-----")
         print("-----4. 점수 확인-----")
         print("-----5. 종    료-----")
-
+        print()
 
     def menu(self):
         self.menu_print()
