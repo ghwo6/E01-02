f5ccbf91ec521c00a372d3406d66af8742d8a528
김호재 <ghwo61351@c6r2s2.codyssey.kr>
2026-08-08 17:20:20 +0900

    FEAT:문제 추가 기능을 추가함 (try except KeyboardInterrupt와 EOF수정)

--- main.py
+++ main.py
@@ -4,11 +4,4 @@ from quizgame import QuizGame
 if __name__ == "__main__":
     quizgame = QuizGame()
     while not quizgame.is_exit_key:
-        try:
-            quizgame.menu()
-        except KeyboardInterrupt:
-            print("\n","ctrl + c를 인식했습니다.")
-            exit(-1)
-
-            
-
+        quizgame.menu()
--- quizgame.py
+++ quizgame.py
@@ -52,17 +52,35 @@ class QuizGame:
                 print("틀렸습니다.")
                 print(f"정답은 {random_quiz.answer_number}입니다.")
                 print("\n\n")
+
+        print(f"최종 점수는 {self.score}입니다.")
+        if self.score > self.highest_score:
+            self.highest_score = self.score
+            print("새로운 기록을 경신하셨습니다!!")
             
     # 메뉴2 문제를 등록합니다.
     def quiz_regist(self):
-        ...
+        new_quiz = None
+        question= s_input("문제를 입력해주세요: >")
+        if question[-1] == "." :
+            question = question[:-1] + "?"
+        elif question[-1] != "?":
+            question = question + "?"
+        option1 = s_input("선택지 1번 : >")
+        option2 = s_input("선택지 2번 : >")
+        option3 = s_input("선택지 3번 : >")
+        option4 = s_input("선택지 4번 : >")
+        answer = int_input("정답을 입력해주세요 : >",1,4)
+        new_quiz = Quiz(question=question,option=[option1,option2,option3,option4],answer_number=answer)
+        self.quiz_list.append(new_quiz)
+        
+
 
     # 메뉴3 문제의 리스트를 봅니다.
     def quiz_list_show(self):
         for i,li in enumerate(self.quiz_list):
-            print(f"{i+1}번 문제")
-            print(li.question_getter())
-        print("\n\n")
+            print(f"{i+1}번 문제 > {li.question_getter()}")
+        print("\n")
 
 
     # 메뉴4 가장 높은 점수를 확인합니다.
@@ -71,34 +89,46 @@ class QuizGame:
 
     # 원하는 정답을 선택하는 기능
     def answer_select(self,quiz:Quiz):
-        try:
-            while True:
+        while True:
 
+            try:
                 select = input("정답(숫자)을 입력해주세요.").strip()
-
-                if select != "" or select is None:
-                    if select.isdigit():
-                        select = int(select)
-                        if 1 <= select <= 4:
-                            return int(select)
-                        else:
-                            print("1 2 3 4 중에 골라주세요.")
-
+            except KeyboardInterrupt:
+                print("\n","(Ctrl + C) 를 입력 받았습니다.","\n")
+                print()
+                exit(-1)
+            except EOFError:
+                print("\n","(Ctrl + D)를 입력 받았습니다.","\n")
+                print()
+                exit(-1)
+            if select != "" or select is None:
+                if select.isdigit():
+                    select = int(select)
+                    if 1 <= select <= 4:
+                        return int(select)
                     else:
-                        print("숫자를 입력해 주세요. (1 ~ 4)")
-                else:
-                    print("'공백'을 입력 받았습니다.")
+                        print("1 2 3 4 중에 골라주세요.")
 
-                quiz.printQuestion()
-        except KeyboardInterrupt:
-            print("(ctrl + c) 를 입력 받았습니다.")
-            exit(-1)
+                else:
+                    print("숫자를 입력해 주세요. (1 ~ 4)")
+            else:
+                print("'공백'을 입력 받았습니다.")
+
+            quiz.printQuestion()
+            
 
     # 원하는 메뉴를 선택하는 기능
     def menu_select(self):
         is_valid_input = False
         while not is_valid_input:
-            select = input("원하시는 번호를 입력해 주세요.").strip()
+            try:
+                select = input("원하시는 번호를 입력해 주세요.").strip()
+            except KeyboardInterrupt:
+                print("\n","(Ctrl + C) 를 입력 받았습니다.","\n")
+                exit(-1)
+            except EOFError:
+                print("\n","(Ctrl + D)를 입력 받았습니다.","\n")
+                exit(-1)
             print()
                 #공백
             if select == "" or select is None:
@@ -147,6 +177,44 @@ class QuizGame:
                 self.is_exit_key = True
 
             # 어떻게 처리할까?
-            case _:
-                self.menu()
+            # case _:
+            #     self.menu()
             
+def s_input(inquiry:str)->str:
+    while True:
+        try:
+            select = input(inquiry).strip()
+        except KeyboardInterrupt:
+            print("\n","(Ctrl + C) 를 입력 받았습니다.","\n")
+            exit(-1)
+        except EOFError:
+            print("\n","(Ctrl + D)를 입력 받았습니다.","\n")
+            exit(-1)
+        if select == "" or select == None:
+            print("아무 입력도 받지 못했습니다.")
+        else:
+            return select
+        
+def int_input(inquiry:str,lower:int,upper:int)->int:
+    while True:
+        try:
+            select = input(inquiry).strip()
+        except KeyboardInterrupt:
+            print("\n","(Ctrl + C) 를 입력 받았습니다.","\n")
+            print()
+            exit(-1)
+        except EOFError:
+            print("\n","(Ctrl + D)를 입력 받았습니다.","\n")
+            print()
+            exit(-1)
+        if select == "" or select == None:
+            print("아무 입력도 받지 못했습니다.")
+        else:
+            if select.isdigit():
+                select = int(select)
+                if lower <= select <= upper:
+                    return select
+                else:
+                    print(f"{lower}  ~ {upper} 사이로 입력해주세요.")
+            else:
+                print("숫자를 입력해주세요.")
